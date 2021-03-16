bab22e15cfac04d1a855ddaddf6fa745697c6674
Documenter.jl <documenter@juliadocs.github.io>
2021-03-16 20:25:35 +0000

    cleanup

--- Precompile.jl
+++
@@ -1,6 +0,0 @@
-using Optics
-@info "Running representative workload"
-# add stuff here #
-Examples.autodrawrays()
-Examples.hexapolarspotdiagramexample()
-@info "Finished running representative workload"
